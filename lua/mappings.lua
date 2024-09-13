f6b055abf90da4571e2298639c39ba5949dde474
Gustaf Wennerholm <gustaf.wennerholm@morningstar.com>
2024-09-13 15:29:28 +0200

    Settings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,8 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+-- map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "<C-j>", "<C-E>")
+map("n", "<C-k>", "<C-Y>")
--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,7 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.opt.listchars = 'space:·,tab:-->,nbsp:␣'
+vim.opt.list = true
+vim.opt.relativenumber = true

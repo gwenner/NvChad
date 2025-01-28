4717b5fc3e2e1049b6f9c8e4c0f01d69931a3528
Gustaf Wennerholm <gustaf.wennerholm@morningstar.com>
2025-01-28 17:12:33 +0100

    Set shell to pwsh if run on Windows.

--- lua/options.lua
+++ lua/options.lua
@@ -5,6 +5,16 @@ require "nvchad.options"
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
-vim.opt.listchars = 'space:·,tab:-->,nbsp:␣'
+vim.opt.listchars = "space:·,tab:-->,nbsp:␣"
 vim.opt.list = true
 vim.opt.relativenumber = true
+
+if vim.loop.os_uname().sysname == "Windows_NT" then
+  vim.opt.shell = "pwsh.exe"
+  vim.opt.shellcmdflag = "-NoLogo -NoProfile -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;$PSStyle.OutputRendering=[System.Management.Automation.OutputRendering]::PlainText;"
+  vim.opt.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
+end
+
